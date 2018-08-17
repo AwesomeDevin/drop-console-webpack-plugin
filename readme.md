daefff4399f78dd7f05d737424d8496e48974513
Devin <daivonup@163.com>
2018-08-17 18:23:12 +0800

    version 2.2.1

--- readme.md
+++ readme.md
@@ -12,13 +12,20 @@ npm install drop-console-webpack-plugin --save
         new DropConsoleWebpackPlugin(),
     ]
 ```
+## Notes
+```
+if   webpack version < 4    :    2.0>drop-console-webpack-plugin version >1.2    
+if   webpack version >= 4   :    drop-console-webpack-plugin version >2.0 && nodejs version > 7.6
+```
 ## Options
+```
 Name | type | default | Description
---------- | ------- | ------ | ---------------------
+---- | ---- | ------- | -----------
 drop_log | Boolean | true | remove console.log(...)
 drop_info | Boolean | true | remove console.info(...)
 drop_warn | Boolean | false | remove console.warn(...)
 drop_error | Boolean | false | remove console.error(...)
+```
 ## Code
 ```
 new DropConsoleWebpackPlugin({
--- replace-test.js
+++ replace-test.js
@@ -1,60 +1,92 @@
-var str = 'console.log(fun())function A(){console.log(123)}console.log()123'
-var str2 = 'console.log'
+class Replace{
+    constructor(str,condition){
+        this.leftStack = []
+        this.rightStack = []
+        this.str = new Buffer(str)
 
-
-leftStack = []
-rightStack = []
-
-
-function matchLeftBracket(str,condition){
-    if(leftStack.length==1)
-    {
-        return str.indexOf('(',leftStack[leftStack.length-1]+12)
     }
-     if(leftStack.length>0)
-    {
-        return str.indexOf('(',leftStack[leftStack.length-1]+1)
+    matchLeftBracket(condition){
+        let leftStack = this.leftStack
+        let rightStack = this.rightStack
+
+        if(leftStack.length==1)
+        {
+            return this.str.toString().indexOf('(',leftStack[leftStack.length-1]+condition.length+2)
+        }
+         if(leftStack.length>0)
+        {
+            return this.str.toString().indexOf('(',leftStack[leftStack.length-1]+1)
+        }
+        return this.str.toString().indexOf(condition+'(')
     }
-    return str.indexOf(condition)
-}
-function matchRightBracket(str){
-    if(rightStack.length>0)
-    {
-        return str.indexOf(')',rightStack[rightStack.length-1]+1)
-    }
-    return str.indexOf(')',leftStack[leftStack.length-1]+1)
-}
-function toReplace(str,condition){
-    let startIndex,endIndex
-    startIndex = matchLeftBracket(str,condition)
-    endIndex = matchRightBracket(str)
-    if(leftStack.length>0&&rightStack.length===leftStack.length)
-    {
-        var strArr = str.split('')
-        strArr.splice(leftStack[0],rightStack[rightStack.length-1]-leftStack[0]+1)
-        str = strArr.join('')
-        leftStack = []
-        rightStack = []
+    matchRightBracket(){
+        let leftStack = this.leftStack
+        let rightStack = this.rightStack
         
-        return toReplace(str,condition)
+        if(rightStack.length>0)
+        {
+            return this.str.toString().indexOf(')',rightStack[rightStack.length-1]+1)
+        }
+        return this.str.toString().indexOf(')',leftStack[leftStack.length-1]+1)
     }
-    if(startIndex>=0&&leftStack.length<1)
+    async main(condition){
+            let leftStack = this.leftStack
+            let rightStack = this.rightStack
+            let startIndex,endIndex
+            startIndex = this.matchLeftBracket(condition)
+            endIndex = this.matchRightBracket()
+            // console.log('----------',condition,startIndex,leftStack,rightStack,this.str.toString())
+            // console.log('++++++++++',leftStack.length>0&&rightStack.length===leftStack.length,startIndex>=0&&leftStack.length<1,startIndex>=0&&startIndex<=endIndex,endIndex>=0 &&leftStack.length>0,startIndex<0&&leftStack.length<1&&rightStack.length<1)
+            if(this.str.toString().length<1)
+            {
+                return this.str.toString()
+            }
+            if(leftStack.length>0&&rightStack.length===leftStack.length)
+            {
+                var strArr = this.str.toString().split('')
+                strArr.splice(leftStack[0],rightStack[rightStack.length-1]-leftStack[0]+1)
+                this.str = new Buffer(strArr.join(''))
+                this.leftStack = []
+                this.rightStack = []
+                return await new Promise((resolve)=>{
+                    process.nextTick(async ()=>{
+                       resolve( await this.main(condition))
+                   })
+                })
+            }
+            if(startIndex>=0&&leftStack.length<1)
+            {
+                leftStack.push(startIndex)
+            }
+            else if(startIndex>=0&&startIndex<=endIndex)
+            {
+                leftStack.push(startIndex)
+            }
+            else if(endIndex>=0 &&leftStack.length>0)
+            {
+                rightStack.push(endIndex)
+            }
+            else if(startIndex<0&&leftStack.length<1&&rightStack.length<1){
+                return  this.str.toString()  
+            }
+            return await new Promise((resolve)=>{
+                process.nextTick(async ()=>{
+                   resolve( await this.main(condition))
+               })
+            })
+    }
+
+    async toReplace(condition)
     {
-        leftStack.push(startIndex)
+        return await this.main(condition)
     }
-    else if(startIndex>=0&&startIndex<=endIndex)
-    {
-        leftStack.push(startIndex)
-    }
-    else if(endIndex>=0 &&leftStack.length>0)
-    {
-        rightStack.push(endIndex)
-    }
-    else if(startIndex<0&&leftStack.length<1&&rightStack.length<1){
-        return str
-    }
-    console.log(str,startIndex,endIndex,leftStack,rightStack)
-    return toReplace(str,condition)
+    
 }
 
-console.log(toReplace(str,'console.log('))
+// new Replace('console.log(fun())function A(){console.log(123)}console.log()//123213console.log(dasdadasdfunction(){a})').toReplace('console.log',(res)=>{
+//     console.log('result',res)
+// })   //test
+// console.log( new Replace('console.log(fun())function A(){console.log(123)}console.log()//123213console.log(dasdadasdfunction(){a})').toReplace('console.log').then((res)=>{
+//     console.log(res)
+// }))
+module.exports = Replace
